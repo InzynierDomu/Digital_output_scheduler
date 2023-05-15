23dd4c426b75825eb9699252fc7d36f5c382885e
Inżynier Domu <inzynier.domu@gmail.com>
2023-05-15 22:39:14 +0200

    add doxygen

--- src/Time_range.h
+++ src/Time_range.h
@@ -6,15 +6,17 @@
  * @date 05-2023
  */
 
+#pragma once
+
 #include <stdint.h>
 struct Time_range
 {
   /*
-  @brief Constructor the Time_range.
-  @param _hour_start Starting hour of the time range.
-  @param _minute_start Starting minute of the time range.
-  @param _hour_stop Ending hour of the time range.
-  @param _minute_stop Ending minute of the time range.
+  @brief Constructor Time_range.
+  @param _hour_start: starting hour of the time range.
+  @param _minute_start: starting minute of the time range.
+  @param _hour_stop: ending hour of the time range.
+  @param _minute_stop: ending minute of the time range.
   */
   Time_range(const uint8_t _hour_start, const uint8_t _minute_start, const uint8_t _hour_stop, const uint8_t _minute_stop)
   : hour_start(_hour_start)
